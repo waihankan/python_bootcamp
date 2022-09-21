fd6d502561414d259f7a407cc75ff2b6b4376033
Wai Han <wh.kankan13@gmail.com>
2022-09-21 13:09:34 -0700

    blank for rock_paper_scissor

---
+++ answer.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Rock paper scissor game
+
+Scissor beats Paper
+Paper beats Rock
+Rock beats Scissor
+
+"""
+import random
+
+
+def number_to_name(number):
+   if number == 1:
+      return "Rock"
+   elif number == 2:
+      return "Paper"
+   elif number == 3:
+      return "Scissor"
+   else:
+      return "Exit"
+
+
+while True:
+    print("Welcome to Rock Paper Scissor Game")
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissor")
+    print("4. Exit")
+
+    user = int(input("Enter your choice: "))
+    user = number_to_name(user)
+    print("You choose     : ", user)
+
+    if user == "Exit":
+       print("See you again")
+       exit()
+
+    else:
+       computer = random.randint(1, 3)
+       computer = number_to_name(computer)
+       print("Computer choose: ", computer)
+
+       if user == "Scissor" and computer == "Rock":
+          print("----Winner: Computer----")
+       elif user == "Scissor" and computer == "Paper":
+          print("----Winner: You----")
+       elif user == "Rock" and computer == "Scissor":
+          print("----Winner: You----")
+       elif user == "Rock" and computer == "Paper":
+          print("----Winner: Computer----")
+       elif user == "Paper" and computer == "Scissor":
+          print("----Winner: Computer----")
+       elif user == "Paper" and computer == "Rock":
+          print("----Winner: You----")
+       elif user == computer:
+          print("----Draw----")
+       else:
+          print("Invalid choice")
--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -21,38 +21,40 @@ def number_to_name(number):
       return "Exit"
 
 
-print("Welcome to Rock Paper Scissor Game")
-print("1. Rock")
-print("2. Paper")
-print("3. Scissor")
-print("4. Exit")
+while True:
+   print("Welcome to Rock Paper Scissor Game")
+   print("1. Rock")
+   print("2. Paper")
+   print("3. Scissor")
+   print("4. Exit")
 
-user = int(input("Enter your choice: "))
-user = number_to_name(user)
-print("You choose     : ", user)
+   user = int(input("Enter your choice: "))
+   user = number_to_name(user)
+   print("You choose     : ", user)
 
-if user == "Exit":
-   print("See you again")
-   exit()
+   if user == "______":
+      print("See you again")
+      exit()
 
-else:
-   computer = random.randint(1, 3)
-   computer = number_to_name(computer)
-   print("Computer choose: ", computer)
-
-   if user == "Scissor" and computer == "Rock":
-      print("----Winner: Computer----")
-   elif user == "Scissor" and computer == "Paper":
-      print("----Winner: You----")
-   elif user == "Rock" and computer == "Scissor":
-      print("----Winner: You----")
-   elif user == "Rock" and computer == "Paper":
-      print("----Winner: Computer----")
-   elif user == "Paper" and computer == "Scissor":
-      print("----Winner: Computer----")
-   elif user == "Paper" and computer == "Rock":
-      print("----Winner: You----")
-   elif user == computer:
-      print("----Draw----")
    else:
-      print("Invalid choice")
+      computer = random.randint(1, 3)      # ask the computer to play the game
+      computer = number_to_name(computer)
+      print("Computer choose: ", computer)
+
+      if user == "Scissor" and computer __ "Rock":
+         print("----Winner: Computer----")
+      elif user __ "Scissor" and computer == "Paper":
+         print("----Winner: You----")
+      elif user == "Rock" and computer == "Scissor":
+         print("----Winner: _____ ----")
+      elif user == "___" and computer == "Paper":
+         print("----Winner: Computer----")
+      elif user == "Paper" and computer == "___":
+         print("----Winner: Computer----")
+      elif _________________________________________:
+         print("----Winner: You----")
+      elif user == computer:
+         print("_____________")
+         print("----Draw----")
+      else:
+         print("Invalid choice")
